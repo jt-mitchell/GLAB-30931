adb789cc7a0332a1e006898a5bb98bb62f315929
Jeff Mitchell <jeffreytm@gmail.com>
2024-12-26 15:38:30 -0500

    first commit

---
+++ src/main/java/mycontroller/ThymeleaftourApplication.java
@@ -0,0 +1,13 @@
+package mycontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleaftourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleaftourApplication.class, args);
+	}
+
+}
